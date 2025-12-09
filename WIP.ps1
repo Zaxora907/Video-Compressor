a21c51f239097fbda59773c99f1c614fd56de7eb
Zaxora <zaxora907@gmail.com>
2025-12-08 18:38:57 -0700

    Add files via upload

--- WIP.ps1
+++ WIP.ps1
@@ -4,9 +4,10 @@ param([string]$IN = $args[0])
 [byte]$AUDIO_BITRATE=128
 [ushort]$MAX_SIZE_KB=9728
 
-[double]$DURATION, [uint]$BITRATE =(ffprobe -i $IN -v error -show_entries format=duration,bit_rate -of csv=p=0) -split ","
+[uint]$BITRATE, [double]$DURATION =(ffprobe -i $IN -v error -select_streams v:0 -show_entries "format=duration:stream=bit_rate" -of csv=p=0) -split ","
+$BITRATE /= 1000
 
 [double]$MAX_VIDEO_BITRATE_KBPS=($MAX_SIZE_KB*8/$DURATION)-$AUDIO_BITRATE
-[double]$FINAL_BITRATE_KBPS = $BITRATE/1000 -lt $MAX_VIDEO_BITRATE_KBPS ? $BITRATE/1000 : $MAX_VIDEO_BITRATE_KBPS
+[double]$FINAL_BITRATE_KBPS = $BITRATE -lt $MAX_VIDEO_BITRATE_KBPS ? $BITRATE : $MAX_VIDEO_BITRATE_KBPS
 
-ffmpeg -i $IN -vf "scale=min(1280\,iw):-1" -c:v libx264 -c:a aac -b:v "${FINAL_BITRATE_KBPS}k" -b:a "${AUDIO_BITRATE}k" $OUT
+ffmpeg -i $IN -vf "scale=min(1280\,iw):-1" -v quiet -stats -c:v libx264 -c:a aac -b:v "${FINAL_BITRATE_KBPS}k" -b:a "${AUDIO_BITRATE}k" $OUT
