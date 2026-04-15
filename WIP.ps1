3743af1c1d727c14b248c8589807647914e8edec
Zaxora <zaxora907@gmail.com>
2026-04-15 16:37:31 -0600

    stupid!
    i added multiple video support.

--- WIP.ps1
+++ WIP.ps1
@@ -1,13 +1,23 @@
-param([string]$IN = $args[0])
-[string]$OUT = "$([System.IO.Path]::GetFileNameWithoutExtension($IN))_converted.mp4"
+param ( 
+    [Parameter(Mandatory=$true, ValueFromRemainingArguments=$true)]
+    [System.IO.FileInfo[]]$VIDEOS
+)
 
-[byte]$AUDIO_BITRATE=128
-[ushort]$MAX_SIZE_KB=9728
+[UInt16]$AUDIO_BITRATE_KBPS = 96
+[UInt16]$MAX_SIZE_KB = 1024 * 9.5
 
-[uint]$BITRATE, [double]$DURATION =(ffprobe -i $IN -v error -select_streams v:0 -show_entries "format=duration:stream=bit_rate" -of csv=p=0) -split ","
-$BITRATE /= 1000
+foreach ($VIDEO in $VIDEOS) {
+    [Double]$DURATION = (ffprobe -i $VIDEO.FullName -v error -show_entries "format=duration" -of csv=p=0)
+    [Double]$VIDEO_BITRATE_KBPS = $MAX_SIZE_KB * 8 / $DURATION - $AUDIO_BITRATE_KBPS
 
-[double]$MAX_VIDEO_BITRATE_KBPS=($MAX_SIZE_KB*8/$DURATION)-$AUDIO_BITRATE
-[double]$FINAL_BITRATE_KBPS = $BITRATE -lt $MAX_VIDEO_BITRATE_KBPS ? $BITRATE : $MAX_VIDEO_BITRATE_KBPS
-
-ffmpeg -i $IN -vf "scale=min(1280\,iw):-1" -v quiet -stats -c:v libx264 -c:a aac -b:v "${FINAL_BITRATE_KBPS}k" -b:a "${AUDIO_BITRATE}k" $OUT
+    ffmpeg -i $VIDEO.FullName `
+        -vf "scale=-2:min(540\iw)," `
+        -c:a libopus -b:a "${AUDIO_BITRATE_KBPS}k" `
+        -c:v libx264 -b:v "${VIDEO_BITRATE_KBPS}k" `
+        -maxrate "${VIDEO_BITRATE_KBPS}k" `
+        -bufsize "$($VIDEO_BITRATE_KBPS*2)k" `
+        -profile:v high -level 4.1 `
+        -x264-params "rc-lookahead=150:ref=6" `
+        -tune animation -movflags +faststart `
+        ($VIDEO.BaseName + "_converted.mp4")
+}
