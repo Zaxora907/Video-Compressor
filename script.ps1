c713326862e11d7116fa296223cb661b76580dc4
Zaxora <zaxora907@gmail.com>
2026-06-06 22:08:34 -0600

    great now i need to update the readme.md
    I added splits in my own stupid way, i also don't like uppercase variables anymore so that'll change later on PROBABLY... i hope i didn't break it.

--- script.ps1
+++ script.ps1
@@ -1,23 +1,26 @@
-param ( 
+param (
     [Parameter(Mandatory=$true, ValueFromRemainingArguments=$true)]
     [System.IO.FileInfo[]]$VIDEOS
 )
 
-[UInt16]$AUDIO_BITRATE_KBPS = 96
-[Double]$MAX_SIZE_MB = 9.5
+[byte]$AUDIO_BITRATE_KBPS = 80
+[Double]$MAX_SIZE_MB = 10
+[byte]$SPLITS = 2
 
 foreach ($VIDEO in $VIDEOS) {
-    [Double]$DURATION = (ffprobe -i $VIDEO.FullName -v error -show_entries "format=duration" -of csv=p=0)
-    [Double]$VIDEO_BITRATE_KBPS = $MAX_SIZE_MB * 8192 / $DURATION - $AUDIO_BITRATE_KBPS
+    [Double]$SEGMENT_DURATION = (ffprobe -i $VIDEO.FullName -v error -show_entries "format=duration" -of csv=p=0) / $SPLITS
+    [Double]$VIDEO_BITRATE_KBPS = $MAX_SIZE_MB * 8192 / $SEGMENT_DURATION - $AUDIO_BITRATE_KBPS
 
-    ffmpeg -i $VIDEO.FullName `
+    for ($I = 0; $I -lt $SPLITS; $I++) {
+        ffmpeg -ss ($SEGMENT_DURATION * $I) -t $SEGMENT_DURATION -i $VIDEO.FullName `
         -vf "scale=-2:min(540\,iw)" `
         -c:a libopus -b:a "${AUDIO_BITRATE_KBPS}k" `
         -c:v libx264 -b:v "${VIDEO_BITRATE_KBPS}k" `
         -maxrate "${VIDEO_BITRATE_KBPS}k" `
-        -bufsize "$($VIDEO_BITRATE_KBPS*2)k" `
+        -bufsize "$($VIDEO_BITRATE_KBPS * 2)k" `
         -profile:v high -level 4.1 -preset veryslow `
         -x264-params "rc-lookahead=240:ref=6" `
         -tune animation -movflags +faststart `
-        ($VIDEO.BaseName + "_converted.mp4")
+        "$($VIDEO.BaseName)_${I}_converted.mp4"
+    }
 }
